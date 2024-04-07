b328244f3743a066e416b91570877d29a964bd7b
federico Lubbert <federico.lubbert@gmail.com>
2024-04-07 12:52:49 +0200

    finished the exersice

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-from flask import Flask, render_template
-
+# main.py
+from flask import Flask, render_template, abort
+from post import Post
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    posts = Post.get_posts()
+    return render_template("index.html", posts=posts)
+
+
+@app.route('/post/<int:blog_id>')
+def blog_post(blog_id):
+    posts = Post.get_posts()
+    post = next((post for post in posts if post['id'] == blog_id), None)
+    if post is None:
+        abort(404)
+    return render_template("post.html", post=post)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
--- post.py
+++ post.py
@@ -1,2 +1,13 @@
+# post.py
+import requests
+
+
 class Post:
-    pass
+    API_URL = "https://api.npoint.io/c790b4d5cab58020d391"
+
+    @classmethod
+    def get_posts(cls):
+        response = requests.get(cls.API_URL)
+        posts = response.json() if response.status_code == 200 else []
+        return posts
+
